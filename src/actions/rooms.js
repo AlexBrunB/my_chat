df1ee0882e4aab6ceb69c13663b525ed6471b44b
abrun <alexandre.brun@epitech.eu>
2018-05-23 12:26:07 +0200

    Change paths

--- src/actions/rooms.js
+++ src/actions/rooms.js
@@ -1,21 +1,21 @@
 import api from '../api';
 
 export function fetchRooms() {
-  return dispatch => api.fetch('rooms')
+  return dispatch => api.fetch('/rooms')
     .then((response) => {
       dispatch({ type: 'FETCH_ROOMS_SUCCESS', response });
     });
 }
 
 export function fetchUserRooms(userId) {
-  return dispatch => api.fetch(`users/${userId}/rooms`)
+  return dispatch => api.fetch(`/users/${userId}/rooms`)
     .then((response) => {
       dispatch({ type: 'FETCH_USER_ROOMS_SUCCESS', response });
     });
 }
 
 export function createRoom(data, router) {
-  return dispatch => api.post('rooms', data)
+  return dispatch => api.post('/rooms', data)
     .then((response) => {
       dispatch({ type: 'CREATE_ROOM_SUCCESS', response });
       router.transitionTo(`r/${response.data.id}`);
@@ -23,7 +23,7 @@ export function createRoom(data, router) {
 }
 
 export function joinRoom(roomId, router) {
-  return dispatch => api.post(`rooms/${roomId}/join`)
+  return dispatch => api.post(`/rooms/${roomId}/join`)
     .then((response) => {
       dispatch({ type: 'ROOM_JOINED', response });
       router.transitionTo(`r/${response.data.id}`);
@@ -33,7 +33,7 @@ export function joinRoom(roomId, router) {
 export function connectToChannel(socket, roomId) {
     return (dispatch) => {
       if (!socket) { return false; }
-      const channel = socket.channel(`rooms:${roomId}`);
+      const channel = socket.channel(`/rooms:${roomId}`);
   
       channel.join().receive('ok', (response) => {
         dispatch({ type: 'ROOM_CONNECTED_TO_CHANNEL', response, channel });
